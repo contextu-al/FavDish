81e5b7146ec48c9b1b6ac5803ca8ed1f277095ab
Daniel Quah <daniel@contextu.al>
2023-03-23 10:24:37 +1100

    Fixed apk location

--- build.sh
+++ build.sh
@@ -64,4 +64,4 @@ LOWERCASE_APP_ENV=$( tr '[A-Z]' '[a-z]' <<< $APP_ENV)
 LOWERCASE_SDK_ENV=$( tr '[A-Z]' '[a-z]' <<< $SDK_ENV)
 
 echo "===== Uploading .apk to AppCenter ====="
-appcenter distribute release --app Contextual/FavDish-"$SDK_ENV"SDK-"$APP_ENV"-"$APP_KEY" --file "$APP_KEY" --group "Collaborators"
+appcenter distribute release --app Contextual/FavDish-"$SDK_ENV"SDK-"$APP_ENV"-"$APP_KEY" --file "$APK_LOCATION" --group "Collaborators"
