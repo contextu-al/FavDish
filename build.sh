81917cd72556c71735ee0add31d53df7f0529e14
Daniel Quah <daniel@contextu.al>
2023-08-22 12:35:08 +1000

    fix compare string instead of assign

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-if [ "$should_setup_emulator" = "true" ]; then
+if [ "$should_setup_emulator" == "true" ]; then
   sdkmanager --install 'system-images;android-TiramisuPrivacySandbox;google_apis_playstore;x86_64'
   sdkmanager --install emulator
   echo "no" | avdmanager --verbose create avd --force --name "contextual_sdk_emulator" --package "system-images;android-TiramisuPrivacySandbox;google_apis_playstore;x86_64"
