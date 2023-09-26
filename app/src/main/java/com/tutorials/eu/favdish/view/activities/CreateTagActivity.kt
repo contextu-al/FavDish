41c61207cbfcaacbee4e1246c1144b765b8273c8
Daniel Quah <daniel@contextu.al>
2023-09-26 11:06:29 +1000

    Use calendar and time picker to format ISO-8601

--- app/src/main/java/com/tutorials/eu/favdish/view/activities/CreateTagActivity.kt
+++ app/src/main/java/com/tutorials/eu/favdish/view/activities/CreateTagActivity.kt
@@ -2,11 +2,18 @@ package com.tutorials.eu.favdish.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.Toast
 import com.contextu.al.Contextual
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
 import com.tutorials.eu.favdish.databinding.ActivityCreateTagBinding
-import java.lang.NumberFormatException
+import java.text.SimpleDateFormat
+import java.time.Instant
 import java.time.OffsetDateTime
+import java.time.ZoneId
+import java.util.Locale
 
 class CreateTagActivity : AppCompatActivity() {
 
@@ -23,6 +30,54 @@ class CreateTagActivity : AppCompatActivity() {
             val value = mBinding.etTagValue.text.toString()
            createTag(tag,value)
         }
+        mBinding.tagSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedTag = mBinding.tagSpinner.selectedItem.toString()
+                mBinding.etTagValue.setText("")
+                if(selectedTag.startsWith("Date Time")){
+                    mBinding.calendarButton.visibility = View.VISIBLE
+                } else {
+                    mBinding.calendarButton.visibility = View.INVISIBLE
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+        mBinding.calendarButton.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select date")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .build()
+            datePicker.show(supportFragmentManager, "datePicker")
+            datePicker.addOnPositiveButtonClickListener { dateFromCalendar ->
+                val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                val date = sdf.format(dateFromCalendar)
+                val timePicker = MaterialTimePicker.Builder()
+                    .setTitleText("Select time")
+                    .build()
+                timePicker.show(supportFragmentManager, "timePicker")
+                timePicker.addOnPositiveButtonClickListener {
+                    mBinding.etTagValue.setText("")
+                    val timePickerHour = if (timePicker.hour <= 9){
+                        "0" + timePicker.hour
+                    } else {
+                        timePicker.hour
+                    }
+                    val timePickerMin = if (timePicker.minute <= 9){
+                        "0" + timePicker.minute
+                    } else {
+                        timePicker.minute
+                    }
+                    val timeZone = ZoneId.systemDefault().rules.getOffset(Instant.now())
+                    val dateTime = date + "T" + timePickerHour + ":" + timePickerMin + ":00" + timeZone
+                    mBinding.etTagValue.setText(dateTime)
+                }
+            }
+
+        }
+
     }
 
     private fun createTag(tag: String, value: String) {
